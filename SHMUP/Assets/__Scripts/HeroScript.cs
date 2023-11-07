c9d9894c864c0733635e6d88bf8a0f035106e1f4
Sean McLaughlin <smm6050@utulsa.edu>
2023-11-07 12:15:27 -0600

    initial commit

---
+++ SHMUP/Assets/__Scripts/HeroScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HeroScript : MonoBehaviour
+{
+    static public HeroScript S { get; private set; }
+    // "S" stands for singleton which is a design concept
+    // {...} is a convenience feature
+    // Only allowing a public get, but set remains private
+    // There will only be one unique Hero
+    [Header("Ship Movement")]
+    public float speed = 30f;
+    public float rollMult = -45f;
+    public float pitchMult = 30;
+
+    [Header("Dynamic")]
+    [Range(0, 4)]
+    public int shieldLevel = 1;
+
+    void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("HeroScript.Awake() - Attempted to create a second HeroScript");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+    }
+}
